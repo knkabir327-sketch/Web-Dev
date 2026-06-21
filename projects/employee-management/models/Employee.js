2c64f28f8131d17a077e2d3add22ab7159e097ed
Abdul Kabir Khan <kingkabir327@gmail.com>
2026-06-22 00:01:22 +0530

    Add files via upload

---
+++ projects/employee-management/models/Employee.js
@@ -0,0 +1,24 @@
+const mongoose= require("mongoose");
+const employeeSchema= new mongoose.Schema({
+    name:{
+        type: String, 
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    department:{
+        type:String,
+        required:true
+    }, 
+    salary:{
+        type:Number,
+        required:true
+    },
+    experience:{
+        type:Number,
+        required:true
+    },
+});
+module.exports= mongoose.model("Employee", employeeSchema);
